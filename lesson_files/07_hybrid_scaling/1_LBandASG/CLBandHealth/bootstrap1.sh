8d31f0d3f2c8107082b690141f57f1c33d3999eb
gokulkrish888 <62772530+gokulkrish888@users.noreply.github.com>
2020-05-18 12:35:36 -0400

    Update bootstrap1.sh

--- lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/bootstrap1.sh
+++ lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/bootstrap1.sh
@@ -7,11 +7,11 @@ cd /var/www/html
 
 wget https://raw.githubusercontent.com/gokulkrish888/content-aws-csa2019/master/lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/Lamar-Jackson-Baltimore-Ravens-1040x572.jpg
 mv Lamar-Jackson-Baltimore-Ravens-1040x572.jpg goat.jpg
-wget https://github.com/linuxacademy/content-aws-csa2019/raw/master/lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/index.php
+wget https://raw.githubusercontent.com/gokulkrish888/content-aws-csa2019/master/lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/index.php
 mv /var/www/html/htaccess /var/www/html/.htaccess
 
 mkdir /var/www/html/goat
-cp /var/www/html/Lamar-Jackson-Baltimore-Ravens-1040x572.jpg /var/www/html/goat
+cp /var/www/html/goat.jpg /var/www/html/goat
 cp /var/www/html/index.php /var/www/html/goat
 cp /var/www/html/.htaccess /var/www/html/goat
 

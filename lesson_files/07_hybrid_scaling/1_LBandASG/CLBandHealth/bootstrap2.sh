c7830a25b32b50548d28d64ce9cb25301f50dd4a
gokulkrish888 <62772530+gokulkrish888@users.noreply.github.com>
2020-05-18 12:29:19 -0400

    Update bootstrap2.sh

--- lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/bootstrap2.sh
+++ lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/bootstrap2.sh
@@ -2,16 +2,16 @@
 yum install -y httpd php git
 
 cd /var/www/html
-wget https://github.com/linuxacademy/content-aws-csa2019/raw/master/lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/cat2.jpg
-mv cat2.jpg cat.jpg
+wget https://github.com/linuxacademy/content-aws-csa2019/raw/master/lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/lebron2.jpg
+mv lebron2.jpg goat.jpg
 wget https://github.com/linuxacademy/content-aws-csa2019/raw/master/lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/index.php
 mv /var/www/html/htaccess /var/www/html/.htaccess
 
 
-mkdir /var/www/html/cat
-cp /var/www/html/cat.jpg /var/www/html/cat
-cp /var/www/html/index.php /var/www/html/cat
-cp /var/www/html/.htaccess /var/www/html/cat
+mkdir /var/www/html/goat
+cp /var/www/html/lebron2.jpg /var/www/html/goat
+cp /var/www/html/index.php /var/www/html/goat
+cp /var/www/html/.htaccess /var/www/html/goat
 
 sudo systemctl start httpd
 sudo systemctl enable httpd
